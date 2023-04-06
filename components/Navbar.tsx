1ab3b2036c5ccb478ee1f030bec71fc684d161b5
shimulsust999 <shimulsust999@gmail.com>
2023-04-07 04:46:58 +0600

    navbar and sidebar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      {" "}
+      <nav className="max-w-[90rem] mx-auto py-4 fixed top-0 w-full left-1/2 -translate-x-1/2 px-4 md:px-0">
+        <p className="navbar">Job Finder Application</p>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
