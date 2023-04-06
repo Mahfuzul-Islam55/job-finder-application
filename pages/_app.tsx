46dbbd0c38636f77d2d4e91aef5523c283d00982
shimulsust999 <shimulsust999@gmail.com>
2023-04-07 05:23:26 +0600

    setting up redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import store from "@/redux/Store";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </Fragment>
+  );
 }
